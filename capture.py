6828330b3a1e70bb0d849c717c0401c4b047a3d8
ravizhan <ravizhan@hotmail.com>
2023-07-14 13:36:11 +0800

    使用yolov8重新训练 更新数据集

--- capture.py
+++ capture.py
@@ -1,18 +1,23 @@
-import json
-
-import torch, base64
-from PIL import Image, ImageOps
-from io import BytesIO
+import base64
+import numpy as np
+import cv2
+from fastdeploy.vision.detection import YOLOv8
 
 
 class Crack:
     def __init__(self):
-        self.model = torch.hub.load('./yolov5', 'custom', path='best.onnx', force_reload=True, skip_validation=True,
-                                    trust_repo=True, source='local')
+        self.model = YOLOv8("./best.onnx")
 
     def inference(self, base64_data):
-        im = Image.open(BytesIO(base64.b64decode(base64_data)))
-        im = ImageOps.expand(im, (0, 2, 12, 0))
-        results = self.model(im, size=512)
-        res = results.pandas().xyxy[0].to_json()
-        return json.loads(res)["xmin"]["0"]
+        img_data = base64.b64decode(base64_data)
+        img_array = np.fromstring(img_data, np.uint8)
+        img = cv2.imdecode(img_array, 1)
+        img = np.array(img).astype("float32")
+        result = self.model.predict(img)
+        # im = fastdeploy.vision.vis_detection(img, result)
+        # cv2.imwrite("result.jpg", im)
+        if len(result.scores) == 1:
+            return result.boxes[0][0]
+        for res in range(len(result.scores)):
+            if result.scores[res] >= 0.9:
+                return result.boxes[res][0]
--- readme.md
+++ readme.md
@@ -6,33 +6,25 @@
 
 ## 特点
 
-- 使用yolov5识别滑动验证码，用魔法打败魔法
+- 使用yolov8识别滑动验证码，用魔法打败魔法
 - 准确率在95%以上，如有需要可自行训练模型
-- 超高性能，i5-3320m识别一次仅需60-70ms
+- 超高性能，CPU识别一次仅需50ms
 - 直接对接官网，数据权威准确
+- 在线体验 [![](https://app.roboflow.com/images/try-model-badge.svg)](https://universe.roboflow.com/ravizhan-xc2or/captcha-border-detection/model/)
 
 ## 使用方法
-
-下载[yolov5项目](https://github.com/ultralytics/yolov5)
-
-```
-git clone https://github.com/ultralytics/yolov5
-```
-
 安装依赖
 
 ```
 pip install -r requirements.txt
 ```
 
+修改`main.py`中要查询的域名 
+
 运行 `main.py` 即可
 
-## 注意事项
+## 备注
 
 - 官网反爬策略较为玄学，请勿疯狂请求
-
-- 项目中有两种格式的模型，CPU推理使用`onnx`更快，GPU推理使用`pt`更快，默认为`onnx`
-
-  位置在`capture.py`
-
-- 本模型使用预训练模型yolov5n进行训练
+- 运行结果为原始响应，如有需要请自行解析
+- 训练集 [![](https://app.roboflow.com/images/download-dataset-badge.svg)](https://universe.roboflow.com/ravizhan-xc2or/captcha-border-detection)
